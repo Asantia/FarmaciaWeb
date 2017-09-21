4bdbcf7083eaddecbf3b811a3f1881e374ccc98d
Asantia <20013051@studenti.uniupo.it>
2017-09-21 16:01:14 +0200

    aggiustato login OB, in corso l'implementazione dell'ivio di messaggi collettivi

--- WebFarmacia/src/actions/NuovoMsgAction.java
+++ WebFarmacia/src/actions/NuovoMsgAction.java
@@ -31,32 +31,121 @@ public class NuovoMsgAction extends Action {
 
         Connection conn = null;
         PreparedStatement st = null;
+        ResultSet rs=null;
 
-        try {
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "$Ultraheroes1");
+        if(emaildest.equals("alltf")){
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "$Ultraheroes1");
 
-            String query="INSERT INTO messaggio (emaildest, oggetto, testo, data, email) VALUES (?,?,?,?,?) " ;
-            st = conn.prepareStatement(query);
-            st.setString(1, emaildest);
-            st.setString(2, oggetto);
-            st.setString(3, testo);
-            st.setDate(4, data);
-            st.setString(5, email);
-            st.executeUpdate();
+                String query="SELECT email from utente where abilitazione='TF'";
+                st = conn.prepareStatement(query);
+                rs = st.executeQuery();
 
-            st.close();
-            conn.close();
-        }
-        catch (Exception e) {
-            System.out.println("Impossibile inserire il nuovo messaggio nel DB: "+ e.getMessage() );
-        }
+                while (rs.next()){
+                    emaildest = rs.getString("email");
 
-        if(u.getAbilitazione().equals("RE"))
+                    try {
+                        PreparedStatement st2 = null;
+                        String query2="INSERT INTO messaggio(emaildest, oggetto, testo, data, email) VALUE (?,?,?,?,?)";
+                        st2 = conn.prepareStatement(query2);
+                        st2.setString(1,emaildest);
+                        st2.setString(2,oggetto);
+                        st2.setString(3,testo);
+                        st2.setDate(4,data);
+                        st2.setString(5,u.getEmail());
+                        st2.executeUpdate();
+                        st2.close();
+                    }
+                    catch (Exception e) {
+                        System.out.println("Errore di connessione al DB: "+ e.getMessage());
+                        return mapping.findForward("failmsg");
+                    }
+                }
+
+                rs.close();
+                st.close();
+                conn.close();
+            }
+            catch (Exception e) {
+                System.out.println("Errore di connessione al DB: "+ e.getMessage());
+                return mapping.findForward("failmsg");
+            }
             return mapping.findForward("messaggiRegione");
-        if(u.getAbilitazione().equals("TF"))
-            return mapping.findForward("messaggiTitolare");
-        else
-            return mapping.findForward("messaggiDipendenti");
+        }
+        else if(emaildest.equals("alldipendenti")){
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "$Ultraheroes1");
+
+                String query="SELECT email from utente where idfarmacia=? AND email<>?";
+                st = conn.prepareStatement(query);
+                st.setInt(1, u.getIdFarmacia());
+                st.setString(2, u.getEmail());
+                rs = st.executeQuery();
+
+                while (rs.next()){
+                    emaildest = rs.getString("email");
+
+                    try {
+                        PreparedStatement st2 = null;
+                        String query2="INSERT INTO messaggio(emaildest, oggetto, testo, data, email) VALUE (?,?,?,?,?)";
+                        st2 = conn.prepareStatement(query2);
+                        st2.setString(1,emaildest);
+                        st2.setString(2,oggetto);
+                        st2.setString(3,testo);
+                        st2.setDate(4,data);
+                        st2.setString(5,u.getEmail());
+                        st2.executeUpdate();
+                        st2.close();
+                    }
+                    catch (Exception e) {
+                        System.out.println("Errore di connessione al DB: "+ e.getMessage());
+                        return mapping.findForward("failmsg");
+                    }
+                }
+
+                rs.close();
+                st.close();
+                conn.close();
+            }
+            catch (Exception e) {
+                System.out.println("Errore di connessione al DB: "+ e.getMessage());
+                return mapping.findForward("failmsg");
+            }
+            if(u.getAbilitazione().equals("TF"))
+                return mapping.findForward("messaggiTitolare");
+            else
+                return mapping.findForward("messaggiDipendenti");
+        }
+        else {
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "$Ultraheroes1");
+
+                String query = "INSERT INTO messaggio (emaildest, oggetto, testo, data, email) VALUES (?,?,?,?,?) ";
+                st = conn.prepareStatement(query);
+                st.setString(1, emaildest);
+                st.setString(2, oggetto);
+                st.setString(3, testo);
+                st.setDate(4, data);
+                st.setString(5, email);
+                st.executeUpdate();
+
+                st.close();
+                conn.close();
+            }
+            catch (Exception e) {
+                System.out.println("Impossibile inserire il nuovo messaggio nel DB: " + e.getMessage());
+                return mapping.findForward("failmsg");
+            }
+
+            if(u.getAbilitazione().equals("RE"))
+                return mapping.findForward("messaggiRegione");
+            else if(u.getAbilitazione().equals("TF"))
+                return mapping.findForward("messaggiTitolare");
+            else
+                return mapping.findForward("messaggiDipendenti");
+        }
     }
 }
